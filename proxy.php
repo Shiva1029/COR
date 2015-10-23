7adb34683f3d5ac48b315da4c0ae17cb298c98f3
Shiva1029 <Shiva1029@users.noreply.github.com>
2015-10-23 09:58:46 -0400

    proxy.php

---
+++ proxy.php
@@ -0,0 +1,28 @@
+<?php
+namespace myProxy;
+
+class HTTP_Proxy {
+
+	public function getURL($url) {
+		preg_match("^https?\:\/\/([a-z\.]+)\/i", $url, $matches);
+		$domain = $matches[0];
+		echo $domain;
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+		"Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7",
+		"Content-Type: text/html", 
+		"Referer: " . $domain, 
+		"Origin: " . $domain,
+		"Authorization: Basic NTAyNDAwNDc4OkVEU0Z1bmNAMjJwd2Q="
+	    ));
+		$html = curl_exec($curl);
+		curl_close ($curl);
+		return $html;
+    }
+
+}
+
+?>
