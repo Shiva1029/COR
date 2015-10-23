5f21627603ae3d1673c2a64eba7c0505f3e4450e
Shiva1029 <Shiva1029@users.noreply.github.com>
2015-10-23 15:13:24 -0400

    Update test.php

--- test.php
+++ test.php
@@ -2,8 +2,17 @@
 
 include_once "proxy.php";
 
+// Decode the input JSON POST data into an object
+$postdata = json_decode(file_get_contents("php://input")); 
+
+// For Associate Array 
+//$postdata = json_decode(file_get_contents("php://input"), true); 
+
 $object = new myProxy\HTTP_Proxy();
-$printer = $object->getURL("url", "basic");
+$printer = $object->getURL($postdata->{'url'}, $postdata->{'oauth'});
+
+// For Associative Array
+// $printer = $object->getURL($postdata["url"], $postdata->["oauth"]);
 
 echo $printer;
 
